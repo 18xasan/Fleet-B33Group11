baa5039e36cfbe80070c69f01db401fe0d98c564
atilboztepe <atilboztepe@gmail.com>
2024-05-13 14:29:44 -0400

    update

--- src/test/java/com/fleet/step_definitions/LoginStepDefs.java
+++ src/test/java/com/fleet/step_definitions/LoginStepDefs.java
@@ -1,87 +1,46 @@
 package com.fleet.step_definitions;
 
+
 import com.fleet.pages.LoginPage;
-import com.fleet.pages.MainPage;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.assertTrue;
-
 public class LoginStepDefs {
 
-    private WebDriver driver;
-    private LoginPage loginPage;
-    private MainPage mainPage;
+   private WebDriver driver;
+   private LoginPage loginPage;
 
-    @Given("I am on the login page")
-    public void iAmOnTheLoginPage() {
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+
         System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
-        loginPage = new LoginPage(driver);
-        driver.get("http://qa.transmuda.com/login");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://qa.transmuda.com/");
+        LoginPage LoginPage = new LoginPage(driver);
     }
 
-    @When("I enter valid {string} credentials")
-    public void iEnterValidCredentials(String role) {
-        String username = "";
-        String password = "";
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String userType) {
+        String username = null;
+        String password = null;
 
-        if (role.equals("Store Manager")) {
-            username = "storemanager51";
-            password = "UserUser123";
-        } else if (role.equals("Sales Manager")) {
-            username = "salesmanager101";
-            password = "UserUser123";
-        } else if (role.equals("Truck Driver")) {
+        // Retrieve username and password based on user type
+        if (userType.equalsIgnoreCase("driver")) {
             username = "user1";
             password = "UserUser123";
+        } else if (userType.equalsIgnoreCase("sales manager")) {
+            username = "salesmanager101";
+            password = "UserUser123";
+        } else if (userType.equalsIgnoreCase("store manager")) {
+            username = "storemanager51";
+            password = "UserUser123";
         }
 
-        loginPage.enterUsername(username);
-        loginPage.enterPassword(password);
     }
 
-    @When("click on the login button")
-    public void clickOnTheLoginButton() {
-        loginPage.clickLoginButton();
-    }
+    @Given("the user logged in with username as {string} and password as {string}")
+    public void the_user_logged_in_with_username_as_and_password_as(String username, String password) {
 
-    @Then("I should be logged in as a {string}")
-    public void iShouldBeLoggedInAsA(String role) {
-        mainPage = new MainPage(driver);
-
-
-        assertTrue(mainPage.isDashboardDisplayed());
-
-
-    }
-
-    @Then("I should see the main modules for {string}")
-    public void iShouldSeeTheMainModulesFor(String role) {
-        mainPage = new MainPage(driver);
-
-        if (role.equals("Store Manager")) {
-            assertTrue(mainPage.isDashboardDisplayed());
-            assertTrue(mainPage.isFleetDisplayed());
-            assertTrue(mainPage.isCustomersDisplayed());
-
-        } else if (role.equals("Sales Manager")) {
-            assertTrue(mainPage.isDashboardDisplayed());
-            assertTrue(mainPage.isSalesDisplayed());
-            assertTrue(mainPage.isActivitiesDisplayed());
-
-        } else if (role.equals("Truck Driver")) {
-            assertTrue(mainPage.isFleetDisplayed());
-            assertTrue(mainPage.isCustomersDisplayed());
-            assertTrue(mainPage.isActivitiesDisplayed());
-        }
-    }
-
-    public void tearDown() {
-        driver.quit();
     }
 }
